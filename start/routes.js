d0272540b223dd819898b9569a0c5ad8eb88a0fa
Thomas Ribeiro <thomas10191@hotmail.com>
2019-11-25 17:14:18 -0200

    add view detail and config routes

--- start/routes.js
+++ start/routes.js
@@ -18,5 +18,7 @@
 const Route = use('Route')
 
 Route.on('/').render('home')
-Route.get('/tasks', 'TaskController.index')
 Route.on('/add').render('add')
+Route.get('/tasks', 'TaskController.index')
+Route.post('add','TaskController.store')
+Route.get('/task/:id', 'TaskController.detail')
